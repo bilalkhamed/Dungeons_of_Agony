80c10acbc351d92c1a711f84af70bf81595d6c75
AbdAbuloha <hakeemabuloha92@outlook.com>
2026-07-29 13:26:26 +0300

    Added header file for Shop

--- Shop.cpp
+++ Shop.cpp
@@ -1,28 +1,23 @@
+#include "Shop.h"
 #include <iostream>
 using namespace std;
 
-class Shop{
-private:
-    Item inventory[7]; // 'Item' Needs to be defined
-    double price[7]; // Needs a size and to be defined
 
-public:
-    void upgradeAttackSpeed() {
-        // Implementation for upgrading attack speed
-    }
+void Shop::upgradeAttackSpeed() {
+     // Implementation for upgrading attack speed
+}
 
-    void upgradeWeapon() {
-        // Implementation for upgrading weapon
-    }
+void Shop::upgradeWeapon() {
+      // Implementation for upgrading weapon
+ }
 
-    void upgradeInventory() {
-        // Implementation for upgrading inventory
-    }
-    void buyItem(int itemId) {
-        // Implementation for buying an item
-    }
+void Shop::upgradeInventory() {
+    // Implementation for upgrading inventory
+}
+void Shop::buyItem(int itemId) {
+    // Implementation for buying an item
+}
 
-    void displayInventory() {
-        // Implementation for displaying inventory
-    }
-};
+void Shop::displayInventory() {
+    // Implementation for displaying inventory
+}
---
+++ Shop.h
@@ -0,0 +1,21 @@
+#ifndef SHOP_H
+#define SHOP_H
+
+#include "Item.h"
+
+class Shop {
+private:
+    // Data members
+    Item inventory[7];
+    double price[7];
+
+public:
+    // Methods
+    void upgradeAttackSpeed();
+    void upgradeWeapon();
+    void upgradeInventory();
+    void buyItem(int itemId);
+    void displayInventory();
+};
+
+#endif // SHOP_H
