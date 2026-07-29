2823751a3cefcf6bb9a899fba86955c58a661619
AbdAbuloha <hakeemabuloha92@outlook.com>
2026-07-29 13:05:43 +0300

    Created Shop.cpp
    Not fully done just the base for the class

---
+++ Shop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Shop{
+private:
+    Item inventory[7]; // 'Item' Needs to be defined
+    double price[7]; // Needs a size and to be defined
+
+public:
+    void upgradeAttackSpeed() {
+        // Implementation for upgrading attack speed
+    }
+
+    void upgradeWeapon() {
+        // Implementation for upgrading weapon
+    }
+
+    void upgradeInventory() {
+        // Implementation for upgrading inventory
+    }
+    void buyItem(int itemId) {
+        // Implementation for buying an item
+    }
+
+    void displayInventory() {
+        // Implementation for displaying inventory
+    }
+};
